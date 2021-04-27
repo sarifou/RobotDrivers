46f791f2b563c55ffe36937ebec19b8717b723c0
sarifou <diallo.msdpro@gmail.com>
2021-04-27 16:19:51 +0200

    Pylint server.py

--- Trains/Serveur/server.py
+++ Trains/Serveur/server.py
@@ -16,23 +16,17 @@ dict_train = {1 : train_1, 2 : train_2, 3: train_3, 4: train_4}
 
 dcc.start()
 
-class thread(threading.Thread) :
+class Thread(threading.Thread):
 
-    def __init__(self, client, address) :
-        
+    def __init__(self, client, address):
         threading.Thread.__init__(self)
-        
         self.client = client
-        
         self.address = address
 
     def run(self):
         data = self.client.recv(1024)
-        
         data = data.decode("utf8")
-
         dict_data = json.loads(data)
-        
         value=0
         state = False
         
@@ -42,13 +36,11 @@ class thread(threading.Thread) :
             state = dict_data["state"]
             
         self.command(dict_data["train"] ,dict_data["command"], value, state)
-        
         client.close()
-        
         print("Fin de la communication ")
         
-    def command(self, number, command, value=0, state=False) :
-        if command == "faster" :
+    def command(self, number, command, value=0, state=False):
+        if command == "faster":
             dict_train[number].faster()
             self.response("Le train {} avance".format(number))
             print(dict_train[number])
@@ -61,33 +53,24 @@ class thread(threading.Thread) :
         elif command == "stop":
             dict_train[number].speed=0
             self.response("Le train {} s'est arrêté".format(number))
-        elif command=="speed":
+        elif command == "speed":
             dict_train[number].speed = value
             self.response("La vitesse du train {} est maintenant {}".format(number, value))
             
     def response(self, message):
-        
         encoded_resp = message.encode()
-        
         client.sendall(encoded_resp)
 
-host, port = ('', 5556)
-
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-socket.bind((host, port))
-
+HOST, PORT = ('', 5556)
+SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+SOCKET.bind((HOST, PORT))
 print("Le serveur est initialisé")
-
 while True:
-    socket.listen(5)
-    client, address = socket.accept()
-
-    print("Un client vient de se connecter {}" .format(address))
-
-    my_thread = thread(client, address)
-    my_thread.start()
-
+    SOCKET.listen(5)
+    CLIENT, ADDRESS = SOCKET.accept()
+    print("Un client vient de se connecter {}" .format(ADDRESS))
+    MY_THREAD = Thread(CLIENT, ADDRESS)
+    MY_THREAD.start()
 
 dcc.stop()
-socket.close()
+SOCKET.close()
