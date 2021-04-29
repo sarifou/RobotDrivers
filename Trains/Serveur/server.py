ddf3e989d1246c0c44236a9d15fc4f0d03250598
sarifou <diallo.msdpro@gmail.com>
2021-04-29 12:15:48 +0200

    Ajout des commandes pour les accessoires

--- Trains/Serveur/server.py
+++ Trains/Serveur/server.py
@@ -13,55 +13,77 @@ train_3 = Train("DCC3", 3)
 train_4 = Train("DCC4", 4)
 
 dict_train = {1 : train_1, 2 : train_2, 3: train_3, 4: train_4}
+dcc.start() # Initialisation du controleur DCC
 
-dcc.start()
+
 
 class Thread(threading.Thread):
+    """
+    """
 
     def __init__(self, client, address):
+        """Fonction pour initialiser les variables de la classe"""
         threading.Thread.__init__(self)
-        self.client = client
-        self.address = address
+        self.client = client #
+        self.address = address #
 
     def run(self):
-        data = self.client.recv(1024)
-        data = data.decode("utf8")
-        dict_data = json.loads(data)
-        value=0
-        state = False
+        """ Fonction qui exécute le processus client """
+        data = self.client.recv(1024) # Reception des données
+        data = data.decode("utf8") # Encodage au format uft8
+        dict_data = json.loads(data) # Transformation au format dictionnaire
+        if "value" in dict_data:
+            self.command_with_value(dict_data["train"], dict_data["value"])
+        elif "state" in dict_data:
+            self.command_with_state(dict_data["train"], dict_data["command"], dict_data["state"]) 
+        else:
+            self.command_simple(dict_data["train"], dict_data["command"])
+        self.client.close() # Fermeture de la communication avec le client
         
-        if dict_data["value"] :
-            value = dict_data["value"]
-        if dict_data["state"] :
-            state = dict_data["state"]
-            
-        self.command(dict_data["train"] ,dict_data["command"], value, state)
-        client.close()
-        print("Fin de la communication ")
-        
-    def command(self, number, command, value=0, state=False):
+    def command_simple(self, number, command):
+        """ Fonction pour exécuter les mouvements de base du train"""
         if command == "faster":
             dict_train[number].faster()
-            self.response("Le train {} avance".format(number))
-            print(dict_train[number])
-        elif command == "slower":
+            self.send_response("Fin")
+        if command == "slower":
             dict_train[number].slower()
-            self.response("Le train {} ralentit".format(number))
-        elif command == "reverse":
+            self.send_response("Fin")
+        if command == "reverse":
             dict_train[number].reverse()
-            self.response("Le train {} change de direction".format(number))
-        elif command == "stop":
+            self.send_response("Fin")
+        if command == "stop":
             dict_train[number].speed=0
-            self.response("Le train {} s'est arrêté".format(number))
-        elif command == "speed":
-            dict_train[number].speed = value
-            self.response("La vitesse du train {} est maintenant {}".format(number, value))
-            
-    def response(self, message):
-        encoded_resp = message.encode()
-        client.sendall(encoded_resp)
+            self.send_response("Fin")
 
-HOST, PORT = ('', 5556)
+    def command_with_value(self, number, value):
+        """Focntion pour les commandes avec valeur"""
+        dict_train[number].speed = value
+        self.send_response("Fin")
+
+    def command_with_state(self, number, command, state):
+        """Fonction pour commander les accessoires des trains"""
+        if command == "fl":
+            dict_train[number].fl = state
+            self.send_response("Fin")
+        elif command == "f1":
+            dict_train[number].f1 = state
+            self.send_response("Fin")
+        elif command == "f2":
+            dict_train[number].f2 = state
+            self.send_response("Fin")
+        elif command == "f3":
+            dict_train[number].f3 = state
+            self.send_response("Fin")
+        elif command == "f4":
+            dict_train[number].f4 = state
+            self.send_response("Fin")
+
+    def send_response(self, message):
+        """Fonction qui renvoie une reponse au client"""
+        encoded_resp = message.encode() # encodage du message au format binaire
+        self.client.sendall(encoded_resp) # envoi du message
+
+HOST, PORT = ('', 5020)
 SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 SOCKET.bind((HOST, PORT))
 print("Le serveur est initialisé")
@@ -69,8 +91,8 @@ while True:
     SOCKET.listen(5)
     CLIENT, ADDRESS = SOCKET.accept()
     print("Un client vient de se connecter {}" .format(ADDRESS))
-    MY_THREAD = Thread(CLIENT, ADDRESS)
-    MY_THREAD.start()
+    MY_THREAD = Thread(CLIENT, ADDRESS) # Instanciation d'un Thread de traitement
+    MY_THREAD.start() # Lancement de la séquence du Thread
 
 dcc.stop()
-SOCKET.close()
+SOCKET.close() # Fermeture de la communication serveur
