291c11ff233b7e4d1878577860ef557c3bfbdf65
sarifou <diallo.msdpro@gmail.com>
2021-05-25 16:42:47 +0200

    Suppression du contrôle PID et nettoyage du code arduino

--- T-quad(AGV)/Arduino_programme/Firmware1.0/Firmware1.0.ino
+++ T-quad(AGV)/Arduino_programme/Firmware1.0/Firmware1.0.ino
@@ -14,8 +14,7 @@
 #include <std_msgs/Float32MultiArray.h>
 #include <ros/time.h>
 #include <sensor_msgs/Range.h>
-//#include <digitalWriteFast.h>
-//#include <EnableInterrupt.h>
+
 
 //============ Définitions et déclarations des moteurs et des codeurs =================
 //Moteur avant gauche
@@ -42,27 +41,6 @@ Motor moteurDroitArriere(36,4,5);
 #define codeurArriereDroitPinA 2
 #define codeurArriereDroitPinB 8
 volatile int8_t ticksCodeurArriereDroit = 0;
-
-//============ Définitions et déclarations des variables pour le PID ==================
-//double Kp = 0.05 ; // gain proportionnel du PID
-//double Ki = 2.5 ; // gain intégral du PID
-//double Kd = 0.1 ;// gain dérivé du PID
-//
-////Moteur avant gauche
-//double InputAVG, OutputAVG, SetPointAVG ;
-//PID moteurAvantGauchePID(&InputAVG, &OutputAVG, &SetPointAVG, Kp, Ki, Kd, DIRECT);
-//
-////Moteur arrière gauche
-//double InputARG, OutputARG, SetPointARG ;
-//PID moteurArriereGauchePID(&InputARG, &OutputARG, &SetPointARG, Kp, Ki, Kd, DIRECT);
-//
-////Moteur avant droit
-//double InputAVD, OutputAVD, SetPointAVD ;
-//PID moteurAvantDroitPID(&InputAVD, &OutputAVD, &SetPointAVD, Kp, Ki, Kd, DIRECT);
-//
-////Moteur arrière droit
-//double InputARD, OutputARD, SetPointARD ;
-//PID moteurArriereDroitPID(&InputARD, &OutputARD, &SetPointARD, Kp, Ki, Kd, DIRECT);
 
 //============ Définitions et déclarations du capteurs ultrasons ======================
 // Déclarations du capteurs ultrason
@@ -102,19 +80,13 @@ void setup() {
   moteurDroitArriere.init();
   moteurGaucheAvant.init();
   moteurGaucheArriere.init();
-//
-//  // Initialisation des encodeurs
-//  initPID();
-//
+
   // Initialisation du handler ros
   nh.getHardware()->setBaud(115200);
   nh.initNode();
   
   // Initilisation du publisher de l'ultrason
   nh.advertise(pub_range);
-
-  // Initilisation du publisher des capteurs de ligne
-//  nh.advertise(pub_linesensor);
   
   // Initialisation du subscriber des moteurs
   nh.subscribe(sub_motors);
@@ -130,11 +102,8 @@ void setup() {
 
 void loop() {
 
-//  SetPointARD = 100 ;
-//  InputARD = ticksCodeurArriereDroit ;
-//  moteurArriereDroitPID.Compute();
   range();
-//  getLineSensors();
+
   nh.spinOnce();
   delay(1000);
 }
@@ -145,75 +114,9 @@ void range() {
   pub_range.publish(&range_msg);
 }
 
-//void getLineSensors() {
-//  line_msg.data[0] = analogRead(ligne_gauche);
-//  line_msg.data[1] = analogRead(ligne_centre);
-//  line_msg.data[2] = analogRead(ligne_droite);
-//  pub_linesensor.publish(&line_msg);
-//}
-
-//void initPID() {
-//  pinMode(codeurAvantDroitPinA, INPUT_PULLUP);
-//  pinMode(codeurAvantDroitPinB, INPUT_PULLUP);
-//  enableInterrupt(codeurAvantDroitPinA, GestionInterruptionCodeurAvantDroitPinA, CHANGE);
-//  moteurAvantDroitPID.SetMode(AUTOMATIC);
-//  moteurAvantDroitPID.SetOutputLimits(-255, 255);
-//  
-//  pinMode(codeurArriereDroitPinA, INPUT_PULLUP);
-//  pinMode(codeurArriereDroitPinB, INPUT_PULLUP);
-//  enableInterrupt(codeurArriereDroitPinA, GestionInterruptionCodeurArriereDroitPinA, CHANGE);
-//  moteurArriereGauchePID.SetMode(AUTOMATIC);
-//  moteurArriereGauchePID.SetOutputLimits(-255, 255);
-//  
-//  pinMode(codeurAvantGauchePinA, INPUT_PULLUP);
-//  pinMode(codeurAvantGauchePinB, INPUT_PULLUP);
-//  enableInterrupt(codeurAvantGauchePinA, GestionInterruptionCodeurAvantGauchePinA, CHANGE);
-//  moteurAvantGauchePID.SetMode(AUTOMATIC);
-//  moteurAvantGauchePID.SetOutputLimits(-255, 255);
-//  
-//  pinMode(codeurArriereGauchePinA, INPUT_PULLUP);
-//  pinMode(codeurArriereGauchePinB, INPUT_PULLUP);
-//  enableInterrupt(codeurArriereGauchePinA, GestionInterruptionCodeurArriereGauchePinA, CHANGE);
-//  moteurArriereGauchePID.SetMode(AUTOMATIC);
-//  moteurArriereGauchePID.SetOutputLimits(-255, 255);
-//}
-//
-//void GestionInterruptionCodeurArriereDroitPinA() {
-//    if (digitalReadFast2(codeurArriereDroitPinA) == digitalReadFast2(codeurArriereDroitPinB)) {
-//        ticksCodeurArriereDroit++;
-//    }
-//    else {
-//        ticksCodeurArriereDroit--;
-//    }
-//
-//}
-//
-//
-//void GestionInterruptionCodeurArriereGauchePinA() {
-//    if (digitalReadFast2(codeurArriereGauchePinA) == digitalReadFast2(codeurArriereGauchePinB)) {
-//        ticksCodeurArriereGauche++;
-//    }
-//    else {
-//        ticksCodeurArriereGauche--;
-//    }
-//}
-//
-//
-//void GestionInterruptionCodeurAvantDroitPinA() {
-//    if (digitalReadFast2(codeurAvantDroitPinA) == digitalReadFast2(codeurAvantDroitPinB)) {
-//        ticksCodeurAvantDroit++;
-//    }
-//    else {
-//        ticksCodeurAvantDroit--;
-//    }
-//}
-//
-//
-//void GestionInterruptionCodeurAvantGauchePinA() {
-//    if (digitalReadFast2(codeurAvantGauchePinA) == digitalReadFast2(codeurAvantGauchePinB)) {
-//        ticksCodeurAvantGauche++;
-//    }
-//    else {
-//        ticksCodeurAvantGauche--;
-//    }
-//}
+void getLineSensors() {
+  line_msg.data[0] = analogRead(ligne_gauche);
+  line_msg.data[1] = analogRead(ligne_centre);
+  line_msg.data[2] = analogRead(ligne_droite);
+  pub_linesensor.publish(&line_msg);
+}
