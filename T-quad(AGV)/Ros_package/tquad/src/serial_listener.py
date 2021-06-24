65712f16880b16c6481ca0b684dd67263ec643cc
sarifou <diallo.msdpro@gmail.com>
2021-06-24 13:24:47 +0200

    LineSensor and BatteryVoltage publisher

--- T-quad(AGV)/Ros_package/tquad/src/serial_listener.py
+++ T-quad(AGV)/Ros_package/tquad/src/serial_listener.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
+# coding: utf-8
 
 #======= Import ================
 import rospy
 from std_msgs.msg import Float64MultiArray
 from sensor_msgs.msg import Range
 from sensor_msgs.msg import BatteryState
+from msg import LineSensor
 
-lines = []
+lineSensor = LineSensor()
 ultrasound = Range()
-battery = BatteryState
+battery = BatteryState()
 
-def range(value):
+def publishRange(value):
+    """
+        Fonction pour publier la valeur renvoyée par le capteur ultrason
+    """
     ultrasound.header.stamp = rospy.Time.now()
     ultrasound.header.frame_id = "/ultrasound"
     ultrasound.radiation_type = 0
@@ -20,26 +25,40 @@ def range(value):
     ultrasound.range = value
     ultrasound_pub.publish(ultrasound)
 
-# --- Subscriber 
-def callback(data):
-    rospy.loginfo("I heard %s",data.data)
-    range(data.data[0])
-    lines.append(data.data[1])
-    lines.append(data.data[2])
-    lines.append(data.data[3])
-    #battery = data.data[4]
-    
+def publishBatteryVoltage(value):
+    """
+        Fonction pour publier le voltage et le pourcentage de la batterie
+    """
+    battery.header.stamp = rospy.Time.now()
+    battery.voltage = value / 1000
+    battery.percentage = 1
+    battery_pub.publish(battery)
 
-    
-def listener():
-    rospy.init_node('serial_bridge')
-    rospy.Subscriber("tquad/serial_publisher", Float64MultiArray, callback)
-    rospy.spin()
+def publishLineSensors(left, middle, right):
+    """
+        Fonction pour publier les valeurs renvoyées par les capteurs de ligne
+    """
+    lineSensor.left = left
+    lineSensor.middle = middle
+    lineSensor.right = right
+    lines_pub.publish(lineSensor)
+
+def callback(data):
+    """
+        Fonction callback du subscriber
+    """
+    rospy.loginfo("I heard %s",data.data)
+    publishRange(data.data[0])
+    publishBatteryVoltage(data.data[4])
+    publishLineSensors(data.data[1], data.data[2], data.data[3])
 
 if __name__ == '__main__':
     try:
+        rospy.init_node('serial_split')
+        rospy.Subscriber("tquad/serial_publisher", Float64MultiArray, callback)
         ultrasound_pub = rospy.Publisher('tquad/ultrasound', Range, queue_size=2)
-        battery_pub = rospy.Publisher('tquad/battery', BatteryState, queue_size=2)
-        listener()
+        lines_pub = rospy.Publisher('tquad/lines_sensors', LineSensor, queue_size=2)
+        battery_pub = rospy.Publisher('tquad/battery_state', BatteryState, queue_size=2)
+        rospy.spin()
     except rospy.ROSInterruptException:
         pass
